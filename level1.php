5f0c5d9a47fe027db0bfff969910fab2a7545296
Yulia N <iuliia_nak@gm.com>
2021-11-24 17:52:44 +0200

    level1.php

---
+++ level1.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', E_ALL);
+error_reporting(E_ALL);
+
+include ('vendor/autoload.php');
+
+$date=date('Y-m-d-H-i-s');
+
+$filename='logs/log' . '-' . $date . '.txt';
+
+
+$webSite =   new \Yulia\Logger\SiteChecker();
+$logger =    new \Yulia\Logger\level1\Logger();
+
+$arrSite = $webSite->getArrWebsitesFromList('inc/Websiteslist.txt');
+
+foreach($arrSite as $site){
+
+    $webSite->setSite($site);
+    $message = $webSite->websiteCheck($site).'<br>';
+    $key = array_search($message,$logger->getArrayAnswer());
+
+    if($key != false){
+        $logger->$key($message,['level'=>$key,'url'=>$site,'filename'=>$filename]);
+    } else{
+    $logger->emergency($message,['level'=>'emergency','url'=>$site,'filename'=>$filename]);
+    }
+
+}
